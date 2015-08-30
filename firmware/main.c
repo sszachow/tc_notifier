ee67783d39c4192d0bb8a7bc216026deadf4ade4
sszachow <sszachow@gmail.com>
2015-08-30 11:35:49 +0200

    Completely modified what pins are set and when.
    This change is to handle new board design.
    Bulbs are connected via relays to PORTD pins 0-2.
    Additionally fixed broken line endings.

--- firmware/main.c
+++ firmware/main.c
@@ -20,15 +20,39 @@ void UARTIntHandler(void) {
     while(ROM_UARTCharsAvail(UART0_BASE)) {
         c = ROM_UARTCharGetNonBlocking(UART0_BASE);
 
-        if(c == 'e') {
+        if(c == '1') {
+            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'1');
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_PIN_0);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        }
+        else if(c == 'q') {
+            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'q');
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, 0);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        }
+        else if(c == '2') {
+            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'2');
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, GPIO_PIN_1);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        }
+        else if(c == 'w') {
+            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'w');
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        }
+        else if(c == '3') {
+            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'3');
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, GPIO_PIN_2);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        }
+        else if(c == 'e') {
             ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'e');
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0, 0);
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
         }
         else {
-            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'d');
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0, GPIO_PIN_0);
+            ROM_UARTCharPutNonBlocking(UART0_BASE, (unsigned char)'?');
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
         }
     }
 }
@@ -39,10 +63,10 @@ int main(void) {
 
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
     ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0);
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2);
 
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
